bcb470a61204cdc649237c45fd277d9813745668
Namir7 <nalbert2012@ya.ru>
2024-08-08 23:33:19 +0300

    temp

--- install-docker.sh
+++ install-docker.sh
@@ -7,3 +7,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Add permissions
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Start new terminal session
